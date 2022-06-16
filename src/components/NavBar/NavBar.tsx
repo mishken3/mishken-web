34c22c48fded7d31975640d3e0304328c0b98dc5
Mikhail <mishken3@gmail.com>
2022-06-16 16:39:44 +0300

    setup navbar links

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,5 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import styles from './NavBar.module.scss';
 
 export const NavBar = () => {
-  return <p>NavBar</p>;
+  return (
+    <div className={styles.navbar}>
+      <p>NavBar</p>
+      <div className={styles.nav__links}>
+        <Link to="/portfolio">portfolio</Link>
+        <Link to="/about">about</Link>
+        <Link to="/contacts">contacts</Link>
+        <Link to="/credits">credits</Link>
+      </div>
+    </div>
+  );
 };
