fdad5bdbdb67c66af414a687f232dbec9011ed8f
Mikhail <mishken3@gmail.com>
2022-06-20 13:40:08 +0300

    refact page layout

--- src/components/Wrapper/Wrapper.tsx
+++ src/components/Wrapper/Wrapper.tsx
@@ -15,10 +15,16 @@ export const Wrapper = () => {
           <div className={styles.page__layout_nav}>
             <NavBar />
           </div>
-          <div>
-            <img className={styles.page__photo} alt="black and white from the back" src={bw} />
+          <div className={styles.page__layout_photo}>
+            <img
+              className={styles.page__layout_photo_img}
+              alt="black and white from the back"
+              src={bw}
+            />
           </div>
-          <Outlet />
+          <div className={styles.page__layout_content}>
+            <Outlet />
+          </div>
         </div>
       </div>
     </>
