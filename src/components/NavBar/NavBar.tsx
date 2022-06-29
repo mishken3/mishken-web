ac68f1804223ef5e70bb69ebb479b5ee13de499b
Mikhail <mishken3@gmail.com>
2022-06-29 18:47:08 +0300

    rework active nav_link style

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,15 +1,33 @@
+import cn from 'classnames';
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 import styles from './NavBar.module.scss';
 
-export const NavBar = () => (
-  <div className={styles.nav}>
-    <div className={styles.nav__links}>
-      <NavLink to="/about">about</NavLink>
-      <NavLink to="/portfolio">portfolio</NavLink>
-      <NavLink to="/photos">photos</NavLink>
-      <NavLink to="/contacts">contacts</NavLink>
+interface GetClassNamesProps {
+  isActive: boolean;
+}
+type GetClassNames = (props: GetClassNamesProps) => string;
+
+export const NavBar = () => {
+  const getClassNames: GetClassNames = ({ isActive }) => cn(isActive && styles.nav__links_active);
+
+  return (
+    <div className={styles.nav}>
+      <div className={styles.nav__links}>
+        <NavLink className={getClassNames} to="/about">
+          about
+        </NavLink>
+        <NavLink className={getClassNames} to="/portfolio">
+          portfolio
+        </NavLink>
+        <NavLink className={getClassNames} to="/photos">
+          photos
+        </NavLink>
+        <NavLink className={getClassNames} to="/contacts">
+          contacts
+        </NavLink>
+      </div>
     </div>
-  </div>
-);
+  );
+};
